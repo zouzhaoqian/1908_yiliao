a5886b35b388c24e102a59fd6cbf4d5862c4b962
An1909634230 <anzihao1909634230@163.com>
2019-12-02 18:02:53 +0800

    对配置文件进行修改，写了一部分后端系统页面

--- src/main/java/com/javahao/controller/UserController.java
+++ src/main/java/com/javahao/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.javahao.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by anzIhao on 2019/12/2.
  */
 @Controller
 public class UserController {
+    @RequestMapping("/houtai")
+    public String show(){
+      return "show";
+    }
 }
